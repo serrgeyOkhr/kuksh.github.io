ac2dbffa41942530bb62d191c1cd1062d606dba3
serrgeyOkhr <ohruamkin.ser@yandex.ru>
2020-04-01 18:46:21 +0700

    identika_readey

--- build/js/identik.js
+++ build/js/identik.js
@@ -15,9 +15,70 @@ $('[data-fancybox="gallery"]').fancybox({
       return '<h3>Тайный показ</h3><p>А здесь краткое описсание того, что ты делал. Что бы прям вообще кайф был</p>';
     }
   });
+$('[data-fancybox="gallery2"]').fancybox({
+    baseClass: "fancybox-custom-layout",
+    infobar: true,
+    touch: {
+      vertical: false
+    },
+    buttons: ["close", "thumbs", "share"],
+    animationEffect: "fade",
+    transitionEffect: "fade",
+    preventCaptionOverlap: false,
+    idleTime: false,
+    gutter: 0,
+    // Customize caption area
+    caption: function(instance) {
+      return '<h3>Тайный показ</h3><p>А здесь краткое описсание того, что ты делал. Что бы прям вообще кайф был</p>';
+    }
+  });
+$('[data-fancybox="gallery3"]').fancybox({
+    baseClass: "fancybox-custom-layout",
+    infobar: true,
+    touch: {
+      vertical: false
+    },
+    buttons: ["close", "thumbs", "share"],
+    animationEffect: "fade",
+    transitionEffect: "fade",
+    preventCaptionOverlap: false,
+    idleTime: false,
+    gutter: 0,
+    // Customize caption area
+    caption: function(instance) {
+      return '<h3>Тайный показ</h3><p>А здесь краткое описсание того, что ты делал. Что бы прям вообще кайф был</p>';
+    }
+  });
+$('[data-fancybox="gallery4"]').fancybox({
+    baseClass: "fancybox-custom-layout",
+    infobar: true,
+    touch: {
+      vertical: false
+    },
+    buttons: ["close", "thumbs", "share"],
+    animationEffect: "fade",
+    transitionEffect: "fade",
+    preventCaptionOverlap: false,
+    idleTime: false,
+    gutter: 0,
+    // Customize caption area
+    caption: function(instance) {
+      return '<h3>Тайный показ</h3><p>А здесь краткое описсание того, что ты делал. Что бы прям вообще кайф был</p>';
+    }
+  });
 
   $(document).ready(function(){
-    $('.your-class').slick({
-      // setting-name: setting-value
+    $('.idn-sliders').slick({
+      arrows: false,
+      responsive: [
+        {
+          breakpoint: 769,
+          settings: {
+            arrows: false,
+            dots: true,
+            slidesToShow: 1
+          }
+        },
+      ]
     });
   });
