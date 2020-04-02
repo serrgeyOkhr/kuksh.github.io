63b0f2f58bcb18b2242fef14c109bb38e4903ddf
serrgeyOkhr <ohruamkin.ser@yandex.ru>
2020-04-02 11:12:59 +0700

    identika ready + struct changes

---
+++ build/js/menu.js
@@ -0,0 +1,19 @@
+
+var menuButton = document.querySelector(".Menu_trigger");
+var closeButton = document.querySelector(".menu_button");
+var popupMenu = document.querySelector(".menu");
+menuButton.addEventListener("click", function cli(event) {
+    event.preventDefault();
+    popupMenu.classList.remove("menu_display")
+    document.body.style.overflow = 'hidden';
+});
+
+function close(event) {
+    event.preventDefault();
+    popupMenu.classList.add("menu_display")
+    document.body.style.overflow = 'visible';
+}
+
+closeButton.addEventListener("click", close);
+
+document.body.addEventListener('touchmove',function(event){event.preventDefault();},false);
